8f36cfda2d242a48035f7d1605dfb520a77bf2ef
CassandraCHZ <19151765@aguascalientes.tecnm.mx>
2024-01-22 18:15:27 -0600

    about me listo y comienzo con el menú

---
+++ src/aboutMe/aboutMe.js
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import './aboutMe.modules.css';
+import React from 'react';
+
+function AboutMe() {
+  const cvUrl= "https://drive.google.com/file/d/1gxadfQkzsf6ZjUDXy17O2MpB1xkBWv3H/view?usp=sharing";
+
+  return (
+    <div className="container">
+      <div className="leftColumn">
+        <div className="header">
+          <span>Hola, soy</span>
+          <p className="texto-subrayado">Cassandra Celheoltl Hernández Zavala</p>
+          <p>
+            Ingeniera en Tecnologías de la Información y Comunicaciones.
+          </p>
+          <p className='text'>
+            Apasionada por el desarrollo web y amante del café. Me caracterizan el compromiso, la disciplina y la responsabilidad. Siempre busco ofrecer resultados excepcionales. ¡Espero que disfrutes revisando mi perfil!
+          </p>
+          <div id="outer">
+            <a className="button_slide slide_right" href={cvUrl} download="CV_CassandraCelheoltlHernándezZavala.pdf" >Descargar CV</a>
+          </div>
+        </div>
+      </div>
+      <div className="rightColumn">
+        <img className="image" src="/images/FotoCirculos.png" alt="Cassandra Hernández"></img>
+        <div className="icon">
+          <a href="https://github.com/tu-usuario-de-github" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="3x"/>
+          </a>
+          <a href="https://github.com/tu-usuario-de-github" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          </a>
+          <a href="https://github.com/tu-usuario-de-github" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faEnvelope} size="3x" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/navigation/navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+import React from 'react';
+import './navbar.modules.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+            <a href="/">Inicio</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
